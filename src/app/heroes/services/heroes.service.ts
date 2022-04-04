db6789f89550b81c22323529ac590561b80fa760
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2022-04-04 02:50:24 +0200

    first commit

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Heroe } from '../interfaces/heroes.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+  private baseURL: string = environment.baseurl
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseURL}/heroes`)
+  }
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.baseURL}/heroes/${id}`)
+  }
+  getSugerencias(termino: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseURL}/heroes?q=${termino}&_limit=6`);
+  }
+}
